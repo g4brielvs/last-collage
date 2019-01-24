aad67f37e18c7cefa6a4b2171cfe6d6f39973c43
Gabriel Stefanini <gabrielusvicente@gmail.com>
2019-01-24 15:04:09 -0500

    Add upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@
 import json
 
 from chalice import Chalice, Response
-from chalicelib import helpers
+from chalicelib.helpers import upload_to_s3
 
 app = Chalice(app_name='app')
 
@@ -18,5 +18,21 @@ def index():
 
     return Response(body=json.dumps(body),
                     status_code=200,
+                    headers={'Content-Type': 'application/json'})
+
+
+@app.route('/upload', methods=['PUT'], content_types=['application/octet-stream'])
+def upload():
+
+    # get raw body of PUT request
+    data = app.current_request.raw_body
+
+    # upload to S3
+    url = upload_to_s3(data) 
+
+    body = {'url' : url}
+    
+    return Response(body=json.dumps(body),
+                    status_code=201,
                     headers={'Content-Type': 'application/json'})
 
--- chalicelib/helpers.py
+++ chalicelib/helpers.py
@@ -3,3 +3,44 @@
 
 
 """helpers.py: Helpers"""
+
+import hashlib
+import os
+import uuid
+
+from io import BytesIO
+
+from boto3 import client 
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+BUCKET = os.environ['BUCKET']
+
+def upload_to_s3(input_data):
+    
+    data = BytesIO()
+    data.write(input_data)
+    data.seek(0)
+
+    S3 = client('s3', 
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name='us-east-1')
+
+    key = uuid.uuid4().hex
+
+    try:
+        S3.upload_fileobj(
+            data,
+            BUCKET,
+            key,           
+            ExtraArgs={
+                "ACL": "public-read",
+                "ContentType": 'image/png'
+            }
+        )
+
+    except Exception as e:
+        return e
+
+    return f'https://s3.amazonaws.com/{BUCKET}/{key}'
